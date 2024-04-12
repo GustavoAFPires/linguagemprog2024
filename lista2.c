8f04d04f84ffe14bcb974ac54c817da9c065fd5d
GustavoAFPires <158634471+GustavoAFPires@users.noreply.github.com>
2024-04-12 04:28:05 +0000

    até a 22

--- lista2.c
+++ lista2.c
@@ -593,23 +593,72 @@ void questao20() {
 //• Tipo de usuário:
 //• Total de dias:
 void questao21() {
+
+	char livro[50];
+	int usuario;
 	
-	
+	printf("Digite o nome do livro: ");
+	scanf(" %50[^\n]", &livro);
+	printf("Escolha o tipo de usuário.\nDigite 1 para professor ou 2 para aluno. ");
+	scanf("%d", &usuario);
+
+	printf("\n");
+
+	if (usuario == 2){
+		printf("• Nome do livro:   %s\n", livro);
+		printf("• Tipo de usuário: Aluno\n");
+		printf("• Total de dias:   3\n");
+	}
+	else if(usuario == 1) {
+		printf("• Nome do livro:   %s\n", livro);
+		printf("• Tipo de usuário: Professor\n");
+		printf("• Total de dias:   10\n");
+	}
+	else {
+		printf("• Tipo de usuário: Inválido\n");
+	}
 }
 
 //22. Construa um programa que leia o percurso em quilómetros, o tipo do carro e informe o consumo estimado
-//de combustível, sabendo-se que um carro tipo C faz 12 km com um litro de gasolina, um tipo B faz 9 kme o tipo C, 8 kmpor litro.
+//de combustível, sabendo-se que um carro tipo A faz 12 km com um litro de gasolina, um tipo B faz 9 km por litro
+//e o tipo C, 8 km por litro.
 void questao22() {
 	
+	float km, consumo;
+	int tipo;
+
+	printf("Digite o percurso em quilómetros: ");
+	scanf("%f", &km);
+
+	printf("Digite o tipo de carro:\n1 para tipo A\n2 para tipo B\n3 para tipo C\n• ");
+	scanf(" %d", &tipo);
+
+	printf("\n");
+
+	if(tipo == 1) {
+		consumo = km / 12;
+		printf("Consumo estimado de combustivel: %.2f litros.\n", consumo);
+	}
+	else if(tipo == 2) {
+		consumo = km / 9;
+		printf("Consumo estimado de combustivel: %.2f litros.\n", consumo);
+	}
+	else if(tipo == 3) {
+		consumo = km / 8;
+		printf("Consumo estimado de combustivel: %.2f litros.\n", consumo);
+	}
+	else {
+		printf("Tipo de carero invalido.\n");
+	}
 }
 
 //23. Crie um programa que informe a quantidade total de calorias de uma refeição a partir da
 //escolha do usuário que deverá informar o prato, a sobremesa, e bebida conforme a tabela a seguir.
-//Prato Sobremesa Bebida
-//Vegetariano 180cal Abacaxi 75cal Chá 20cal
-//Peixe 230cal Sorvete diet 110cal Suco de laranja 70cal
-//Frango 250cal Mousse diet 170cal Suco de melão 100cal
-//Carne 350cal Mousse chocolate 200cal Refrigerante diet 65cal
+//Prato                Sobremesa                 Bebida
+//Vegetariano 180cal   Abacaxi 75cal             Chá 20cal
+//Peixe 230cal         Sorvete diet 110cal       Suco de laranja 70cal
+//Frango 250cal        Mousse diet 170cal        Suco de melão 100cal
+//Carne 350cal         Mousse chocolate 200cal   Refrigerante diet 65cal
 void questao23() {
 
 }
